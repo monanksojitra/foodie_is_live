51e7093fa56e0e86e6505c46a3a4d2acc1918378
monank sojitra <monaksojitra4444@gmail.com>
2024-03-05 08:08:37 +0530

    add: deploy workflow

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,7 @@
-import React from 'react'; 
-import { Route, BrowserRouter, Routes  } from 'react-router-dom'; 
-import Home from './components/Home'; 
-import About from './components/About'; 
-import NotFound from './components/NotFound'; 
-import MainLayout from './layout/MainLayout'; 
-import './App.css'; 
-function App() { 
-  return ( 
-    <BrowserRouter> 
-      <MainLayout> 
-        <Routes> 
-          <Route index path="/" Component={Home} /> 
-          <Route path="/about" Component={About} /> 
-          <Route Component={NotFound} /> 
-        </Routes> 
-      </MainLayout> 
-    </BrowserRouter> 
-  ); 
-} 
- 
-export default App; 
+import Home from "./components/Home";
+import "./App.css";
+function App() {
+  return <Home />;
+}
+
+export default App;
