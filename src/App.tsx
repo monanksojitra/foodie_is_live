849f67cf81a73c0dd9d6990da75b901ae20c40bf
monank sojitra <monaksojitra4444@gmail.com>
2024-03-05 09:12:23 +0530

    [fix] bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import Home from "./components/Home";
 import "./App.css";
+import MainLayout from "./layout/MainLayout";
 function App() {
-  return <Home />;
+  return (
+    <MainLayout>
+      <Home />
+    </MainLayout>
+  );
 }
 
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/foodie_live/",
+  base: "/foodie_is_live/",
   plugins: [react()],
 });
